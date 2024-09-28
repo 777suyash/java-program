e06a891ccb3c92a52481f2c9de11633ece0b0fc5
Suyash Bhosale <suyashbhosale1305@gmail.com>
2024-09-28 15:23:10 +0530

    code

--- Loop/for loop/p30.java
+++ Loop/for loop/p30.java
@@ -1,7 +1,7 @@
 class p30{
 	public static void main(String args[]){
-		char ch='A';
 		for(int i=1; i<=3; i++){
+			char ch='A';
 			for(int j=1; j<=4; j++){
 				System.out.print(ch+" ");
 				ch++;
